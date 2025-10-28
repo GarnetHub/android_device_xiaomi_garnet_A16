5123b71b4733f8e6815dea7eade7eb54e88588fb
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-10-28 14:05:45 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,8 +6,14 @@ git clone https://github.com/MySelly/android_hardware_xiaomi_garnet hardware/xia
 echo 'Cloning display'
 rm -rf hardware/qcom-caf/sm8450/display && git clone https://github.com/MySelly/hardware_qcom-caf_sm8450_display hardware/qcom-caf/sm8450/display
 
-echo 'Cloning camera'
-git clone https://github.com/stuff-garnet/vendor_xiaomi_miuicamera-garnet vendor/xiaomi/miuicamera-garnet && git clone https://github.com/stuff-garnet/device_xiaomi_miuicamera-garnet device/xiaomi/miuicamera-garnet
-
 echo 'Cloning dolby'
 git clone https://github.com/MySelly/android_hardware_dolby -b 16.0 hardware/dolby
+
+echo 'Cloning kernel'
+git clone https://github.com/crdroidandroid/android_xiaomi_kernel_garnet -b 16.0 kernel/xiaomi/garnet
+
+echo 'Cloning kernel modules'
+git clone https://github.com/MySelly/android_kernel_xiaomi_garnet-modules -b lineage-23.0 kernel/xiaomi/garnet-modules
+
+echo 'Cloning kernel devicetrees'
+git clone https://github.com/MySelly/android_kernel_xiaomi_garnet-devicetrees -b lineage-23.0 kernel/xiaomi/garnet-devicetrees
